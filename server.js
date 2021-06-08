6e53f546a7650d33e37fd297413b658983021bb7
rishav1802 <rishabh.kumar1802@gmail.com>
2021-06-08 20:48:22 +0530

    prepare deployment

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import cors from "cors"
 import restaurants from "./api/restaurants.route.js"
 
 const app = express()
-// const path = require('path');
+const path = require('path');
 
 app.use(cors())
 app.use(express.json())
@@ -14,6 +14,13 @@ app.use("*", (req, res) => res.status(404).json
 
 //Server Stattic assets in production
 
-// if(process.env.)
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+  
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
 
 export default app
