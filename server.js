662c9eacc93131db7fb098048072f21c92a43b33
rishav1802 <rishabh.kumar1802@gmail.com>
2021-06-08 20:38:54 +0530

    prep deploy

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import cors from "cors"
 import restaurants from "./api/restaurants.route.js"
 
 const app = express()
+// const path = require('path');
 
 app.use(cors())
 app.use(express.json())
@@ -10,5 +11,9 @@ app.use(express.json())
 app.use("/api/v1/restaurants", restaurants)
 app.use("*", (req, res) => res.status(404).json
 ({ error: "not found"}))
+
+//Server Stattic assets in production
+
+// if(process.env.)
 
 export default app
